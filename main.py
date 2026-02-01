6fc6eae111f866316c88f9fea24c557a3a18a3fa
pamakdivyesh181-creator <pamakdivyesh181@gmail.com>
2026-02-01 10:32:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print("Sales Demand Forecasting Project Started")
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+
+# Load data
+data = pd.read_csv("data/sample.csv")
+
+# Convert Month to numbers
+data["Month_Number"] = range(1, len(data) + 1)
+
+# Prepare input (X) and output (y)
+X = data[["Month_Number"]]
+y = data["Sales"]
+
+# Train model
+model = LinearRegression()
+model.fit(X, y)
+
+# Predict next month sales
+next_month = [[len(data) + 1]]
+prediction = model.predict(next_month)
+
+print("Past Sales Data:")
+print(data[["Month", "Sales"]])
+
+print("\nPredicted sales for next month:", int(prediction[0]))
