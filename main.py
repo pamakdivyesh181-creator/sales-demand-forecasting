89d8ff6859d9ca90eeedfb068410e575fbebefb7
pamakdivyesh181-creator <pamakdivyesh181@gmail.com>
2026-02-02 22:03:11 +0530

    Implement sales trend graph visualization
    Added a sales graph plot using matplotlib.

--- main.py
+++ main.py
@@ -23,3 +23,12 @@ print("Past Sales Data:")
 print(data[["Month", "Sales"]])
 
 print("\nPredicted sales for next month:", int(prediction[0]))
+
+import matplotlib.pyplot as plt
+
+# Plot sales graph
+plt.plot(data["Month"], data["Sales"], marker='o')
+plt.xlabel("Month")
+plt.ylabel("Sales")
+plt.title("Monthly Sales Trend")
+plt.show()
